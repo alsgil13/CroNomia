078ebfe7a63c4a6aacb0a528fe54cfd3d27d6b27
alsgil13 <als.gil13@gmail.com>
2019-12-18 16:46:59 -0300

    Arruma responsividade do index

--- cronomia/loopstudy/views.py
+++ cronomia/loopstudy/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from loopstudy.models import Cor, Ciclo, Disciplina, TipoDisciplina, Profile, Sprint
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
 def index(request):
@@ -12,28 +14,47 @@ def perfil(request):
     context = {}
     return render(request,'perfil.html', context=context)
 
-class CicloListView(generic.ListView):
+
+class CicloListView(LoginRequiredMixin, generic.ListView):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'    
     model = Ciclo
+    
+    def get_queryset(self):
+        return Ciclo.objects.filter(dono=Profile.objects.filter(user=self.request.user)[0])
     #alterar queryset depois do sistema de login para retornar apenas os ciclos do usuário
 
-class CicloDetailView(generic.DetailView):
+class CicloDetailView(LoginRequiredMixin, generic.DetailView):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'    
     model = Ciclo
 
 #Forms
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
-class CicloCreate(CreateView):
+class CicloCreate(LoginRequiredMixin, CreateView):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'    
+    model = Ciclo
+    
+    fields = ['nome','descricao']
+    success_url = reverse_lazy('ciclos')
+
+    def form_valid(self, form):
+        form.instance.dono = Profile.objects.filter(user=self.request.user)[0]
+        return super().form_valid(form)    
+
+class CicloUpdate(LoginRequiredMixin, UpdateView):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'    
     model = Ciclo
     fields = ['nome','descricao']
-    success_url = reverse_lazy('ciclo')
+    success_url = reverse_lazy('ciclos')
 
-class CicloUpdate(UpdateView):
+class CicloDelete(LoginRequiredMixin, DeleteView):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'    
     model = Ciclo
-    fields = ['nome','descricao']
-    success_url = reverse_lazy('ciclo')
-
-class CicloDelete(DeleteView):
-    model = Ciclo
-    success_url = reverse_lazy('ciclo')
+    success_url = reverse_lazy('ciclos')
     #disciplinas = Disciplina.objects.filter(ciclo = Ciclo.id)
